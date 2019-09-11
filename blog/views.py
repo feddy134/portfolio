8cf41b0ac10a01db6a6f79fd48f721a20bb73a61
feddy134 <fednish@gmail.com>
2019-09-11 13:15:13 +0530

    Polished

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+def allblogs(request):
+    allblog = Blog.objects
+
+    return render(request,'blog/allblogs.html',{'allblog': allblog})
